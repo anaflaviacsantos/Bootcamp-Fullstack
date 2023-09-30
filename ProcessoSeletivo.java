56a58936221b06d09898417b472048d880734ec6
Ana <anafcs.profissional@gmail.com>
2023-09-29 23:04:11 -0300

    Refatoração: atualizar Processo Seletivo
    Ajustes nos métodos de cadastrar e selecionar candidatos

--- ProcessoSeletivo.java
+++ ProcessoSeletivo.java
@@ -2,12 +2,28 @@ import java.util.Scanner;
 
 public class ProcessoSeletivo {
 
-    Scanner scanner = new Scanner(System.in);
-    public static void main (String[] args){
+    public static String[] CadastrarCandidatos(){
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número de candidatos: ");
+        int qtdeCandidatos = scanner.nextInt(); 
+        scanner.nextLine();
+        
+        String[] candidatos = new String[qtdeCandidatos];
+
+        for(int i = 0; i < qtdeCandidatos; i++){
+            System.out.println("Digite o nome do candidato " + (i+1)  + ": ");
+            candidatos[i] = scanner.nextLine(); 
+            
+        }
+
+        scanner.close();
+        
+        return candidatos; 
     }
 
-    public boolean AnalisarCandidato(double salarioPretendido){
+    public static boolean AnalisarCandidato(double salarioPretendido){
 
         double salarioBase= 2000;
 
@@ -26,20 +42,9 @@ public class ProcessoSeletivo {
 
     }
 
-    public void CadastrarCandidatos(){
+    public static String[] SelecionarCandidatos(String[] candidatos){
 
-        System.out.println("Digite o número de candidatos: ");
-        int qtdeCandidatos = scanner.nextInt(); 
-
-        String[] candidatos = new String[qtdeCandidatos];
-
-        for(int i = 0; i < qtdeCandidatos; i++){
-            System.out.println("Digite o nome do candidato " + (i+1)  + ": ");
-            candidatos[i] = scanner.nextLine(); 
-        }
-    }
-
-    public void SelecionarCandidatos(String[] candidatos){
+         Scanner scanner = new Scanner(System.in);
 
          int qtdeSelecionados = 0, candidatoAtual = 0;
          String[] candidatosSelecionados = new String[5];
@@ -47,22 +52,47 @@ public class ProcessoSeletivo {
          while(qtdeSelecionados < 5){
 
             System.out.println("Salário pretendido do candidato " + (candidatoAtual + 1) +" (" + candidatos[candidatoAtual] + "): ");
-            double salarioPretendido = scanner.nextDouble(); 
+                double salarioPretendido = scanner.nextDouble(); 
+                scanner.nextLine();
 
-            if(AnalisarCandidato(salarioPretendido)){
+                if(AnalisarCandidato(salarioPretendido)){
 
-                System.out.println( candidatos[candidatoAtual] + " foi selecionado(a)!");
+                    System.out.println( candidatos[candidatoAtual] + " foi selecionado(a)!");
 
-                candidatosSelecionados[qtdeSelecionados]= candidatos[candidatoAtual]; 
-                qtdeSelecionados++; 
-            }
-            else{
-               System.out.println( candidatos[candidatoAtual] + " não foi selecionado(a)"); 
-            }
+                    candidatosSelecionados[qtdeSelecionados]= candidatos[candidatoAtual]; 
+                    qtdeSelecionados++; 
+                }
+                else{
+                System.out.println( candidatos[candidatoAtual] + " não foi selecionado(a)"); 
+                }
 
-            candidatoAtual++; 
-        }
+                candidatoAtual++; 
+                }
 
+        scanner.close();
+
+        return candidatosSelecionados;    
     }
  
+    public static void MostrarCandidatos(String[] selecionados){
+
+        System.out.println("Os candidatos selecionados foram: ");
+        for(int i = 0; i < selecionados.length; i++){
+            System.out.println(selecionados[i]);
+        }
+    }
+
+    public static void main (String[] args){
+
+        String[] candidatos = CadastrarCandidatos(); 
+        String[] selecionados = SelecionarCandidatos(candidatos);
+
+        MostrarCandidatos(selecionados);
+
+
+    }
+
+    
+
+    
 }
